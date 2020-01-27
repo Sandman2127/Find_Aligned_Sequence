d400498be7762c76749e01eceee7efa93ec14310
Dean Sanders <Sandman2127@gmail.com>
2020-01-27 11:54:19 -0600

    Adds parser for an input list, automated current working directory detection

--- Find_Seq_in_Sam.py
+++ Find_Seq_in_Sam.py
@@ -4,25 +4,25 @@ import argparse, re, os
 
 parser = argparse.ArgumentParser(description='Parse sam file for sequence')
 parser.add_argument('-inputSam', type=str, help='i.e.: /path/to/your/sam/file.sam', required=True)
+parser.add_argument('-inputList', type=str, help='i.e.: /path/to/your/sam/list.txt', required=False)
 
 args = parser.parse_args()
 
 SAMFILE = args.inputSam
-testList = ['GTTTACCCGCCAATATATCCTGTCA','TAGGC','TAGATAAAG','AAAGAGGATACTG','GTTTACACCACAATATATCCTGCCA','TTGATCCCGAGGGGAACCCTGTGGT','GAGGGGTTGGATCAAAGTACT','CTGAAAGTTGACCCGCTTCATGG','CAGCTTGGTGTGATCCTCCGCCGGCAA','GCCTTGGCGTACCGCGTACATCTT','CAGATAGCAGCTCGGTAATGGTCTT','ATTTACTCTGGTAGCTGCGCGATGTATT','TCATCTACTCATTTATTCATTTGCTC','CCATGCCGCCTCCTTTAGCCGCTAAAAT','TCGGTGCCTGGTTGTTCTTGATTTT','CCACACATGGGGCATTCCACGGCG','TGTGGACGGAACACGCGGCCGGGCTTGTC','GGATCACCTCGCCAGCTCGTCGGTCA','GCGGTTGCCGGGATTCTTTGCGGATTCGA','ACTTCTCCACCAGGTCATCACCCAG','GCCGCGCCGATTTGTACCGGGCCGGA','CAGGTCGTAATCCCACACACTGGC','CTCTTTTCTCTTAGGTTTACCCGCCA','AATATCCGTTATTCTAATAAACG','ATAAACCTTTTCACGCCCTTTTA','TTGGCATGCACATACAAATGGACGAACGG','TTGATCCCGAGGGGAACCCTGTGG','GAACCCTGTGGTTGGCATGCACA','TATATCCTGTCAAACACTGATAGTTTA','CTAATAAACGCTCTTTTCT','CACTCAGAAGAACTCGTCAAGAAGGC']
+LISTFILE = args.inputList
 
-# SAM format
-# Col	Field	Type	Brief description
-# 1	QNAME	String	Query template NAME
-# 2	FLAG	Int	bitwise FLAG
-# 3	RNAME	String	References sequence NAME
-# 4	POS	Int	1- based leftmost mapping POSition
-# 5	MAPQ	Int	MAPping Quality
-# 6	CIGAR	String	CIGAR String
-# 7	RNEXT	String	Ref. name of the mate/next read
-# 8	PNEXT	Int	Position of the mate/next read
-# 9	TLEN	Int	observed Template LENgth
-# 10	SEQ	String	segment SEQuence
-# 11	QUAL	String	ASCII of Phred-scaled base QUALity+33
+testList = ['GTTTACCCGCCAATATATCCTGTCA','GTTTACACCACAATATATCCTGCCA','TTGATCCCGAGGGGAACCCTGTGGT','GAGGGGTTGGATCAAAGTACT','CTGAAAGTTGACCCGCTTCATGG','CAGCTTGGTGTGATCCTCCGCCGGCAA','GCCTTGGCGTACCGCGTACATCTT','CAGATAGCAGCTCGGTAATGGTCTT','ATTTACTCTGGTAGCTGCGCGATGTATT','TCATCTACTCATTTATTCATTTGCTC','CCATGCCGCCTCCTTTAGCCGCTAAAAT','TCGGTGCCTGGTTGTTCTTGATTTT','CCACACATGGGGCATTCCACGGCG','TGTGGACGGAACACGCGGCCGGGCTTGTC','GGATCACCTCGCCAGCTCGTCGGTCA','GCGGTTGCCGGGATTCTTTGCGGATTCGA','ACTTCTCCACCAGGTCATCACCCAG','GCCGCGCCGATTTGTACCGGGCCGGA','CAGGTCGTAATCCCACACACTGGC','CTCTTTTCTCTTAGGTTTACCCGCCA','AATATCCGTTATTCTAATAAACG','ATAAACCTTTTCACGCCCTTTTA','TTGGCATGCACATACAAATGGACGAACGG','TTGATCCCGAGGGGAACCCTGTGG','GAACCCTGTGGTTGGCATGCACA','TATATCCTGTCAAACACTGATAGTTTA','CTAATAAACGCTCTTTTCT','CACTCAGAAGAACTCGTCAAGAAGGC']
+
+def parse_list():
+    outList = []
+    with open(LISTFILE,"r") as inputF:
+        for line in inputF:
+            line = line.strip()
+            line = line.split()
+            outList.extend(line)
+    return outList
+
+
 def make_output_dir(path,access_rights):
     try:
         os.mkdir(path, access_rights)
@@ -31,12 +31,12 @@ def make_output_dir(path,access_rights):
     else:
         print ("Successfully created the directory %s" % path)
 
-def parse_sam(path):
+def parse_sam(path,input_list):
     # open a log file to describe the operations of the find
     logFileName = str(path) + "Regex_find_log.txt"
     logFile = open(logFileName,"w+")
 
-    for FINDSEQ in testList:
+    for FINDSEQ in input_list:
 
         # open new output sam file in output directory:
         fileName = str(path) + str(FINDSEQ) + ".sam"
@@ -59,8 +59,20 @@ def parse_sam(path):
                 #header
                 if line[0][0:1] == '@':
                     fileOut.write(cat_and_print_line(line))
-
-                #sam format lines
+                
+                # SAM format lines
+                # Col	Field	Type	Brief description
+                # 1	QNAME	String	Query template NAME
+                # 2	FLAG	Int	bitwise FLAG
+                # 3	RNAME	String	References sequence NAME
+                # 4	POS	Int	1- based leftmost mapping POSition
+                # 5	MAPQ	Int	MAPping Quality
+                # 6	CIGAR	String	CIGAR String
+                # 7	RNEXT	String	Ref. name of the mate/next read
+                # 8	PNEXT	Int	Position of the mate/next read
+                # 9	TLEN	Int	observed Template LENgth
+                # 10	SEQ	String	segment SEQuence
+                # 11	QUAL	String	ASCII of Phred-scaled base QUALity+33
                 else:
                     QNAME,FLAG,RNAME,POS,MAPQ,CIGAR,RNEXT,PNEXT,TLEN,SEQ,QUAL = line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10]
                     total += 1 
@@ -98,13 +110,21 @@ def cat_and_print_line(lineA):
 
 if __name__ == "__main__":
     #TODO: define output directory
-    path_output_dir = "./output_sams/"
+    cwd = os.getcwd()
+    path_output_dir = str(cwd) + "/output_sams/"
+
     #TODO: define the access rights
-    access_rights = 0o755
+    access_rights = 0o775
     #TODO: make output directory
     make_output_dir(path_output_dir,access_rights)
-    #TODO: parse your sam and output hits to output directory
-    parse_sam(path_output_dir)
+    
+    #TODO: determine if using a list or the built in test list, and parse your sam and output hits to output directory
+    if LISTFILE:
+        print("Parsing sam file for user defined input sequence list")
+        output_List = parse_list()
+        parse_sam(path_output_dir,output_List)
+    else:
+        parse_sam(path_output_dir,testList)
 
     
 
